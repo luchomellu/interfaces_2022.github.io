66a3ed0795eaf249eb5d5d7bdd365403669ec732
Luciano Melluso <luchomellu@live.com.ar>
2022-10-06 03:09:56 -0300

    feat: :technologist: Se trabajo en muchas cosas (Header, Menu, Cards, Carruseles, etc)

--- TP2/js/carousel.js
+++ TP2/js/carousel.js
@@ -2,11 +2,14 @@ const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const carousel = document.querySelector(".carousel-container");
 const track = document.querySelector(".track");
+
 let width = carousel.offsetWidth;
 let index = 0;
+
 window.addEventListener("resize", function () {
   width = carousel.offsetWidth;
 });
+
 next.addEventListener("click", function (e) {
   e.preventDefault();
   index = index + 1;
@@ -16,6 +19,7 @@ next.addEventListener("click", function (e) {
     next.classList.add("hide");
   }
 });
+
 prev.addEventListener("click", function () {
   index = index - 1;
   next.classList.remove("hide");
---
+++ TP2/js/menu.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector(".fa-bars");
+
+menu.addEventListener("click", function () {
+    document.querySelector(".nav-menu").classList.toggle("mostrar");
+  });
