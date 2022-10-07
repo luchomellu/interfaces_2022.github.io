c8f18574a0a06ca6bd8488f87dd04b8fe5b26677
Luciano Melluso <luchomellu@live.com.ar>
2022-10-07 04:52:54 -0300

    feat: :rocket: empezamos loader

--- TP2/js/menu.js
+++ TP2/js/menu.js
@@ -1,5 +1,18 @@
 const menu = document.querySelector(".fa-bars");
+const loader = document.querySelector(".loader-wrapper");
 
 menu.addEventListener("click", function () {
-    document.querySelector(".nav-menu").classList.toggle("mostrar");
+  document.querySelector(".nav-menu").classList.toggle("mostrar");
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+  //loader.classList.add("loader-hidden");
+  //loader.addEventListener("transitionend", () =>{
+  //  this.document.body.removeChild("loader-wrapper");
+  
   });
+
+  setTimeout(() => { loader.classList.add("loader-hidden");}, 4000);
+//target.addEventListener('click', () => target.style.opacity = '0');
+// If you want to remove it from the page after the fadeout
+//target.addEventListener('transitionend', () => target.remove());
