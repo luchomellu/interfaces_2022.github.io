37bf21dffdf14290a9b1cfdb96819d8a3dcb5cae
Luciano Melluso <luchomellu@live.com.ar>
2022-11-24 23:42:07 -0300

    feat: :zap: falta poco

--- TP4/js/reveal.js
+++ TP4/js/reveal.js
@@ -32,6 +32,21 @@ function reveal(){
         }
     }
 
+    let container = document.querySelector('.container');
+    let headline = document.querySelector('#headline');
+    let countdown = document.querySelector('#countdown');
+    let windowHeight = window.innerHeight;
+    let revealTop = container.getBoundingClientRect().top;
+    let revealPoint = 1;
+    
+    if(revealTop < windowHeight * revealPoint && (container.getBoundingClientRect().top - windowHeight / 2) > 0){
+        console.log((revealTop - windowHeight / 2) * 0.1);
+        countdown.style.transform = `translateY(${revealTop - windowHeight / 2}%)`;
+        countdown.style.opacity = `${100 - (revealTop - windowHeight / 2) * 0.5}%`;
+        headline.style.transform = `translateY(-${container.getBoundingClientRect().top - windowHeight / 2}%)`;
+        headline.style.opacity = `${100 - (revealTop - windowHeight / 2) * 0.5}%`;
+    }
+
     function assignImg(i){
         current = i;
         if(i == 0){
@@ -44,4 +59,29 @@ function reveal(){
             img.src = "./images/feature3.png";
         }
     }
+    
 }
+
+const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+release = 12 + "/" + 25 + "/" + 2022;
+today = 11 + "/" + 24 + "/" + 2022;
+
+const countDown = new Date(release).getTime(),
+    x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        if (distance < 0) {
+        clearInterval(x);
+        }
+    }, 0)
